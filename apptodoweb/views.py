38ec9765b89e898c9014301ca28c9e2b8b5dd44d
unknown <yusufzbayram@outlook.com>
2024-12-15 19:22:49 +0300

    gorsel duzenlemeler yapildi

--- apptodoweb/views.py
+++ apptodoweb/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-
 from .forms import TodoForm
 from .models import Model_ToDoWeb
 from django.contrib import messages
@@ -26,19 +25,6 @@ def index(request):
     print(page)
     return render(request, 'apptodoweb/index.html', page)
 
-
-"""def view_addToDo(request):
-    #item_list = Model_ToDoWeb.objects.order_by("-date")
-    if request.method == "POST":
-        title = request.POST.get("title")
-        detail = request.POST.get("detail")
-        date = request.POST.get("date")
-        my_data = Model_ToDoWeb(title=title,
-                                detail=detail,
-                                date=date)
-        my_data.save()
-    my_data_list = Model_ToDoWeb.objects.all()  
-    return render(request, 'todo/index.html', {view_addToDo: my_data_list})"""
 
 
 ### function to remove item, it receive todo item_id as primary key from url ##
